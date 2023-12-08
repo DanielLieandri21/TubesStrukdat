0ce23a1a5b5533e67c62df0bb0798d2315ce8525
DanielLieandri21 <134904382+DanielLieandri21@users.noreply.github.com>
2023-12-08 21:40:00 +0700

    Tubes.cpp

--- Tubes.cpp
+++ Tubes.cpp
@@ -65,10 +65,44 @@ void insertAfterfilm(listfilm &L, addressfilm P, addressfilm Y, addressfilm Prec
         while (next(Prec) != Y){
             Prec = next(Prec);
         }
-        next(P) = Y;
+        next(P) = next(Prec);
         prev(P) = Prec;
         next(Prec) = P;
-        prev(Y) = P;
+        prev(next(Prec)) = P;
         relasi(L) = Q;
+    }
+}
+void insertFirstartis(listartis &L, addressartis P, addressrelasi Q){
+    if (first(L) == NULL){
+        first(L) = P;
+    }else{
+        next(P) = first(L);
+        first(L) = P;
+        Q = nextartis(P);
+    }
+}
+void insertLastartis(listartis &L, addressartis P, addressartis Y, addressrelasi Q){
+    if (first(L) == NULL){
+        first(L) = P;
+    }else{
+        Y = first(L);
+        while (next(Y) != NULL){
+            Y = next(Y);
         }
+        next(Y) = P;
+        Q = nextartis(P);
+    }
+}
+void insertAfterartis(listartis &L, addressartis P, addressartis Y, addressartis Prec, addressrelasi Q){
+    if (first(L) == NULL){
+        first(L) = P;
+    }else{
+        Prec = first(L);
+        while (next(Prec) != Y){
+            Prec = next(Prec);
+        }
+        next(P) = next(Prec);
+        next(Prec) = P;
+        Q = nextartis(P);
+    }
 }
